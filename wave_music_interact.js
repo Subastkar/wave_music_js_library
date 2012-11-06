82e362239a522829e314509c9344238a84576188
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2012-11-06 00:27:32 -0600

    Playlists and songs interactions

--- wave_music_interact.js
+++ wave_music_interact.js
@@ -1,38 +1,61 @@
-// var X = function(){
-
-//   var getSongs = function(callback){
-//     $.getJSON("/api/v1/asset/", function(data){
-//       return callback(data);
-//     });
-//   };
-
-//   return {
-//     getSongs: function(){
-
-//       getSongs(function(s){ 
-//         this.songs = s;
-//       });
-
-//     }
-//   };
-// };
-
-//var songs = new X().getSongs();
-
-var X = function(){
+var wave_music = function(){
+  BASE_URI = "http://rubyfloyd.r12.railsrumble.com"
 
   var getSongs = function(callback){
-    $.getJSON("/api/v1/asset/", function(data){
+    $.getJSON(BASE_URI + "/api/songs",function(data){
       return callback(data);
     });
   };
 
+  var getSong = function(id, callback){
+    $.getJSON(BASE_URI + "/api/songs/" + id, function(data){
+      return callback(data);
+    });
+  };
+
+  var getPlaylists = function(callback){
+    $.getJSON(BASE_URI + "/api/playlists", function(data){
+      return callback(data);
+    });
+  };
+
+  var getPlaylist = function(id, callback){
+    $.getJSON(BASE_URI + "/api/playlists/" + id, function(data){
+      return callback(data);
+    });
+  };
+
+  var createPlaylist = function(data){
+    $.ajax({
+      type: "POST",
+      url: BASE_URI + "/api/playlists",
+      data: data
+    }).done(function( msg ) {
+      alert( "Data Saved: " + msg );
+    });
+  };
+
+  var addSongs = function(id, data){
+    $.ajax({
+      type: "POST",
+      url: BASE_URI + "/api/playlists/" + id + "/add_songs",
+      data: data
+    }).done(function( msg ) {
+      alert( "Data Saved: " + msg );
+    });
+  };
+
   return {
-   getSongs: getSongs
+   getSongs:       getSongs,
+   getSong:        getSong,
+   getPlaylists:   getPlaylists,
+   getPlaylist:    getPlaylist,
+   createPLaylist: createPlaylist, 
+   addSongs:       addSongs
   };
 
 };
 
 
 
-var songs = new X();
+var wm = new wave_music();
